c5f2fe30f310e33201e6de1ad8a24976baf3331a
leignel <cristian.leignel@gmail.com>
2023-10-03 23:30:00 -0300

    Primeiro commit

---
+++ biblioteca/src/main/java/com/residencia/biblioteca/entities/Editora.java
@@ -0,0 +1,75 @@
+package com.residencia.biblioteca.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "editora")
+public class Editora {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigoeditora")
+	private Integer codigoEditora;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "imagem_nome")
+	private String imagemNome;
+	
+	@Column(name = "imagem_filename")
+	private String imagemFileName;
+	
+	@Column(name = "imagem_url")
+	private String imagemUrl;
+
+	public Integer getCodigoEditora() {
+		return codigoEditora;
+	}
+
+	public void setCodigoEditora(Integer codigoEditora) {
+		this.codigoEditora = codigoEditora;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImagemNome() {
+		return imagemNome;
+	}
+
+	public void setImagemNome(String imagemNome) {
+		this.imagemNome = imagemNome;
+	}
+
+	public String getImagemFileName() {
+		return imagemFileName;
+	}
+
+	public void setImagemFileName(String imagemFileName) {
+		this.imagemFileName = imagemFileName;
+	}
+
+	public String getImagemUrl() {
+		return imagemUrl;
+	}
+
+	public void setImagemUrl(String imagemUrl) {
+		this.imagemUrl = imagemUrl;
+	}
+	
+	
+	
+	
+}
